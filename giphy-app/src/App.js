0506e94a4feb2bcd529da3f6d4b2edc837c1ef3a
Brittany Cho <bac411@nyu.edu>
2020-01-09 15:00:39 -0500

    initial App.js

--- giphy-app/src/App.js
+++ giphy-app/src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import './App.css';
- 
+
+// class SearchBar extends Component {
+//     constructor(props) {
+// 	    super(props);
+
+//       this.state = {
+//           data: this.props.data
+//       }
+	
+// 	    this.search = this.search.bind(this);
+//     }
+
+//     search() {
+//       axios.get("http://ctp-zip-api.herokuapp.com/city/" + (document.getElementById("inputCity").value).toUpperCase())
+//           .then((response) => {
+//             console.log(response);
+//             console.log(typeof response);
+//             console.log(response.data);
+//             console.log(response.data[0]);
+            
+//             this.setState({ data: response.data });
+            
+
+//         })
+//           .then((error) => {
+//             console.log(error);
+//         })
+//     }
+
+//     render() {
+
+// 	return (
+    
+// 		<div>
+// 		City Searcher <br/>
+// 		<input type="text" id="inputCity"/> <br/>
+// 		<button onMouseDown={this.search}> Search </button>
+// 		{/* <div> {this.state.data} </div> */}
+//     {this.state.data.map(zip => <div key={zip}><h3>{zip}</h3></div>)}
+// 		</div>
+// 		);
+		
+//     }
+// }
+
+class SearchBar extends Component {
+    constructor(props) {
+	    super(props);
+
+      this.state = {
+        data: this.props.data
+      }
+	
+	   
+    }
+
+    render() {
+
+      return (
+        
+        <div>
+          <h1>Test</h1>
+        </div>
+        );
+		
+    }
+}
+
+SearchBar.propTypes = {
+    data: PropTypes.array
+};
+
+SearchBar.defaultProps = {
+    data: []
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+	    <div class="App-header">
+	      <SearchBar/>
+	     </div>
+	     );
 }
 
 export default App;
+
+ 
